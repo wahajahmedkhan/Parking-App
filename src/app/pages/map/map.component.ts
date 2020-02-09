15c7d66d4fe458f456d51a7a305790158ccdf569
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2020-02-09 11:18:24 +0500

    Google map View changes

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -11,20 +11,39 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   map: google.maps.Map;
 
-  lat = 40.730610;
-  lng = -73.935242;
+  lat = 24.8607;
+  lng = 67.0011;
 
   coordinates = new google.maps.LatLng(this.lat, this.lng);
 
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
-    zoom: 8,
+    zoom: 12,
   };
 
-  marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
-  });
+  places = [
+    {
+      id: 1,
+      name: 'Bahadurabad',
+      description: 'Parking Around the Charminar',
+      lat: '24.8823',
+      long: '67.0673'
+    },
+    {
+      id: 2,
+      name: 'Dolmen mall clifton',
+      description: 'Parking Outside the Mall',
+      lat: '24.802505',
+      long: '67.029456'
+    },
+    {
+      id: 3,
+      name: 'Burns Road',
+      description: 'Parking around the famous food street',
+      lat: '24.8554',
+      long: '67.0174'
+    }
+  ];
 
 
   constructor() {
@@ -36,14 +55,17 @@ export class MapComponent implements OnInit, AfterViewInit {
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement,
       this.mapOptions);
-
-    this.marker.setMap(this.map);
   }
 
 
   ngAfterViewInit() {
     this.mapInitializer()
+  }
 
+  viewArea(area) {
+    const point = new google.maps.LatLng(area.lat, area.long);
+    this.map.setCenter(point);
+    this.map.setZoom(16)
   }
 
 }
